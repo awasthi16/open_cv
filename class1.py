d6d618a85f915f36bb61f4dbacb8d0c92608769a
durgesh awashti <133563743+awasthi16@users.noreply.github.com>
2025-10-08 22:11:23 +0530

    Create class1.py

---
+++ class1.py
@@ -0,0 +1,9 @@
+import cv2
+
+img = cv2.imread('image.jpg')  # or cv2.IMREAD_COLOR
+cv2.imshow('Color Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+https://colab.research.google.com/drive/1UyRyxnZN1AQCpivQDzWanAyOgB3nKmKu?usp=sharing
